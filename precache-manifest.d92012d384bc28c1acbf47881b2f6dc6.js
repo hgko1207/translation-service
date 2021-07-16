feeca4fbcf84fc31a8dea68b9495d9d7853cabe6
hgko1207 <khkkhk1207@gmail.com>
2021-07-16 13:45:55 +0900

    Updates

---
+++ precache-manifest.d92012d384bc28c1acbf47881b2f6dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f850c2073e52ec2d18fb168e993deac1",
+    "url": "/translation-service/index.html"
+  },
+  {
+    "revision": "d5a17acf20749ef304c1",
+    "url": "/translation-service/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "392d7a19824c9404236e",
+    "url": "/translation-service/static/css/main.6f63c26b.chunk.css"
+  },
+  {
+    "revision": "d5a17acf20749ef304c1",
+    "url": "/translation-service/static/js/2.62d3686f.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/translation-service/static/js/2.62d3686f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392d7a19824c9404236e",
+    "url": "/translation-service/static/js/main.02e3c7e3.chunk.js"
+  },
+  {
+    "revision": "76966428b835511aff14",
+    "url": "/translation-service/static/js/runtime-main.263c79aa.js"
+  }
+]);
